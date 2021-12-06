ee8268388fdec431ab44f88f679b0952c8c197ef
macbook pro <macbookpro@MacBook-Pro-de-macbook.local>
2021-12-06 21:53:00 +0100

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Rick' },
+  { id: 12, name: 'Michonne' },
+  { id: 13, name: 'Rosita' },
+  { id: 14, name: 'Princess' },
+  { id: 15, name: 'The King' },
+  { id: 16, name: 'Judith' },
+  { id: 17, name: 'Maggie' },
+  { id: 18, name: 'Daryl' },
+  { id: 19, name: 'Carol' },
+  { id: 20, name: 'Eugene' },
+];
